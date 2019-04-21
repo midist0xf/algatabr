37be1d86527d696c6fc667176a87b94e3a41a40e
mike <m.distefanompaf@gmail.com>
2019-04-21 16:42:31 +0200

    main

---
+++ algaABR/src/app/Main.java
@@ -0,0 +1,32 @@
+package app;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/view/mainview.fxml"));
+			primaryStage.setTitle("AlgaT - ABR");
+			primaryStage.setScene(new Scene(root, 700, 400));
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
+
+
+	
+
---
+++ algaABR/src/controller/ABRViewController.java
@@ -0,0 +1,38 @@
+package controller;
+
+public class ABRViewController {
+	
+	public void handleRandomClick()  {
+		
+	}
+	public void handleStepClick() {
+	
+	}
+	public void handleInsertClick() {
+		
+	}
+	public void handleRemoveClick() {
+		
+	}
+	public void handleSuccessorClick() {
+		
+	}
+	public void handlePredecessorClick() {
+		
+	}
+	public void handleMinClick() {
+		
+	}
+	public void handleMaxClick() {
+		
+	}
+	
+	public void handleLookupClick() {
+		
+	}
+	
+	public void handleRunClick() {
+		
+	}
+
+}
