b37813de7ae1b386805fc2c5cf815438b861c07c
mike <m.distefanompaf@gmail.com>
2019-04-21 18:08:03 +0200

    Alert input

--- algaABR/src/controller/ABRViewController.java
+++ algaABR/src/controller/ABRViewController.java
@@ -1,5 +1,11 @@
 package controller;
 
+import java.util.Optional;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextInputDialog;
+
 public class ABRViewController {
 	
 	public void handleRandomClick()  {
@@ -9,6 +15,26 @@ public class ABRViewController {
 	
 	}
 	public void handleInsertClick() {
+		
+		TextInputDialog inputNodeDialog = new TextInputDialog();
+		
+		inputNodeDialog.setTitle("AlgaT - ABR");
+		inputNodeDialog.setHeaderText("Inserisci la chiave:");
+		inputNodeDialog.setContentText("Valore chiave:");
+		 
+		Optional<String> result = inputNodeDialog.showAndWait();
+		 
+		result.ifPresent(key -> {
+			/* check if is an integer */
+			if (isStringInt(key)) {
+				System.out.print(key);				
+			} else {
+				showAlert("L'input inserito non è un intero!");
+				
+			}
+		    
+		});
+		
 		
 	}
 	public void handleRemoveClick() {
@@ -33,6 +59,34 @@ public class ABRViewController {
 	
 	public void handleRunClick() {
 		
+	}
+	
+	
+	
+	/* I metodi che seguono svolgono funzioni ausiliarie */
+	/* TODO: probabilmente da rendere static e inserire in una classe Utility */
+	private void showAlert(String s) {
+		Alert alert = new Alert(AlertType.ERROR);
+		 
+		alert.setTitle("Error alert");
+	 	alert.setHeaderText(null);
+		alert.setContentText(s);
+		 
+		alert.showAndWait();		
+	}
+	
+	
+	
+	private boolean isStringInt(String s)
+	{
+	    try
+	    {
+	        Integer.parseInt(s);
+	        return true;
+	    } catch (NumberFormatException ex)
+	    {
+	        return false;
+	    }
 	}
 
 }
