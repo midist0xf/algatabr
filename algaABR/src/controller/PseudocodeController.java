6d998f9e26e200d3e216334e3f62a4cecfa1c3ff
giovifazi <giovifazi@gmail.com>
2019-04-29 15:52:00 +0200

    Sistemata grafica pseudocode e aggiunto metodo per evidenziare riga

--- algaABR/src/controller/PseudocodeController.java
+++ algaABR/src/controller/PseudocodeController.java
@@ -1,8 +1,11 @@
 package controller;
 
+import java.awt.TextField;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 import org.json.simple.JSONObject;
 
@@ -10,17 +13,26 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 public class PseudocodeController {
 	
 	@FXML private ComboBox<String> functionsCBox;
-	@FXML private TextArea codeTextArea;
+	@FXML private TextFlow codeTextFlow;
 	
 	private Hashtable<String, String> functionsHTable = new Hashtable<String, String>();
 	
 	@FXML
 	public void handleShowCode() {
-		codeTextArea.setText(functionsHTable.get(functionsCBox.getValue()));
+		codeTextFlow.getChildren().clear();
+
+		Text t = new Text(functionsHTable.get(functionsCBox.getValue()));
+		codeTextFlow.getChildren().add(t);
+		
+		highlightLine(3);
 	}
 
 	public void loadCodes(JSONObject jFunctions) {
@@ -36,6 +48,22 @@ public class PseudocodeController {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	// NB le righe di codice sono numerate a partire da 0 
+	public void highlightLine(int lineNumber) {
+		Pattern regex = Pattern.compile("\\n");
+		String[] stringSplit = regex.split(functionsHTable.get(functionsCBox.getValue()));
+		Text[] textSplit = new Text[stringSplit.length];
+		
+		for (int i = 0; i < stringSplit.length; i++) {
+			textSplit[i] = new Text(stringSplit[i] + "\n");
+		}
+		
+		textSplit[lineNumber].setFill(Color.CHOCOLATE);
+		textSplit[lineNumber].setFont(new Font("Times New Roman", 17));
+		
+		codeTextFlow.getChildren().setAll(textSplit);
 	}
 	
 }
